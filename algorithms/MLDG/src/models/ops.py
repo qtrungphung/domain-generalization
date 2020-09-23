d7f82af7d179ba6f1244efc8275a7001a7964805
qtrungphung <trungphung.123@gmail.com>
2020-09-23 04:55:39 +0000

    add model elements

---
+++ algorithms/MLDG/src/models/ops.py
@@ -0,0 +1,76 @@
+import torch.autograd as autograd
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+def linear(inputs, weight, bias,
+           meta_step_size=0.001, meta_loss=None, stop_gradient=False):
+    if meta_loss is not None:
+
+        if not stop_gradient:
+            grad_weight = autograd.grad(meta_loss, weight, create_graph=True)[0]
+
+            if bias is not None:
+                grad_bias = autograd.grad(meta_loss, bias, create_graph=True)[0]
+                bias_adapt = bias - grad_bias * meta_step_size
+            else:
+                bias_adapt = bias
+
+        else:
+            grad_weight = autograd.grad(meta_loss, weight, create_graph=True)[0]
+            grad_weight.requires_grad_(False)
+
+            if bias is not None:
+                grad_bias = autograd.grad(meta_loss, bias, create_graph=True)[0]
+                grad_bias.requires_grad_(False)
+                bias_adapt = bias - grad_bias * meta_step_size
+            else:
+                bias_adapt = bias
+
+        return F.linear(inputs,
+                        weight - grad_weight * meta_step_size,
+                        bias_adapt)
+    else:
+        return F.linear(inputs, weight, bias)
+
+
+def conv2d(inputs, weight, bias,
+           stride=1, padding=0, dilation=1, groups=1,
+           meta_loss=None, meta_step_size=0.001, stop_gradient=False):
+    if meta_loss is not None:
+
+        if not stop_gradient:
+            grad_weight = autograd.grad(meta_loss, weight, create_graph=True)[0]
+
+            if bias is not None:
+                grad_bias = autograd.grad(meta_loss, bias, create_graph=True)[0]
+                bias_adapt = bias - grad_bias * meta_step_size
+            else:
+                bias_adapt = bias
+
+        else:
+            grad_weight = autograd.grad(meta_loss, weight, create_graph=True)[0]
+            grad_weight.requires_grad_(False)
+
+            if bias is not None:
+                grad_bias = autograd.grad(meta_loss, bias, create_graph=True)[0]
+                grad_bias.requires_grad_(False)
+                bias_adapt = bias - grad_bias * meta_step_size
+            else:
+                bias_adapt = bias
+
+        return F.conv2d(inputs,
+                        weight - grad_weight * meta_step_size,
+                        bias_adapt, stride,
+                        padding,
+                        dilation, groups)
+    else:
+        return F.conv2d(inputs, weight, bias, stride, padding, dilation, groups)
+
+
+def relu(inputs):
+    return F.threshold(inputs, 0, 0, inplace=True)
+
+
+def maxpool(inputs, kernel_size, stride=None, padding=0):
+    return F.max_pool2d(inputs, kernel_size, stride, padding=padding)
